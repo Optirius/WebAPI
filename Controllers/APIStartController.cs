2401c9b7b5600271e4c76fd7beb114e08f60cadf
Optirius <tahmid95.aquarius@gmail.com>
2022-07-30 19:20:21 +0600

    Proper Start Page

---
+++ Controllers/APIStartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class APIStartController : ControllerBase
+    {
+
+        public APIStartController()
+        {
+            Start();
+        }
+
+
+        public IActionResult Start()
+        {
+            return Ok("API has been started!");
+        }    
+
+    }
+}
